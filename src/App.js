3e2569e52bae054349a4e2f511279852edeeaad8
Kenneth <Kenneth.p.huang@gmail.com>
2019-03-18 09:42:24 +1000

    Build the client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      response: false,
+      endpoit: 'http://127.0.0.1:8008'
+    };
+  }
+
+  componentDidMount(){
+    const {endpoit} = this.state;
+    const socket = socketIOClient(endpoit);
+    socket.on('FromAPI', data=>{
+      console.log('get data:'+data);
+      this.setState({response:data.temp_c})
+    });
+  }
   render() {
+    const {response} = this.state;
+    console.log('response:'+response)
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +39,9 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div style={{textAlign:"center"}}>
+          {response?<p>the temperature is {response} &#176;C</p>:<p>loading...</p>}
+        </div>
       </div>
     );
   }
